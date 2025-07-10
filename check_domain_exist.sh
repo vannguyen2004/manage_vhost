ec351c277b1e746e590fa62ca3c9b2ff047f61e9
vannguyen2004 <vannguyen2004cn@gmail.com>
2025-07-10 21:01:24 +0700

    Update check_domain_exist.sh

--- check_domain_exist.sh
+++ check_domain_exist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 domain=$1
-if [[ -f "/etc/apache2/sites-available/${domain}.conf" ]] ; then
+if [[ -f "/etc/apache2/sites-available/${domain}.conf" ]] || [[ -f "/etc/apache2/sites-available/${domain}-le-ssl.conf" ]] ; then
         exit 0
 else
         exit 1
